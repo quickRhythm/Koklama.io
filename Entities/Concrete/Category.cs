e48656676a15d93a9f32343ea4fd11e4413ab447
Selçuk Bilgen <selcukbilgen@hotmail.com>
2021-01-31 22:39:58 +0300

    database entity classes added

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,10 @@
+using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Category : IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
